164fccbe8c39b027a7ccee684e5a62840a29ec56
Tanjin Ahamed <tanjin15-9185@diu.edu.bd>
2021-02-05 21:23:06 +0600

    time

--- timeout.js
+++ timeout.js
@@ -5,8 +5,18 @@ function doSomething(){
 }
 
 console.log(2);
-setTimeout(doSomething, 1000); //onno kaj hote takbe e time er por eta execute krbe
+setTimeout(doSomething, 4000); //onno kaj ses hye gele e time er por eta execute krbe/onno kaj ses hote e time teke besi lagle oita ses por eta execute hbe
 console.log(4);
 console.log(4);
+setTimeout(function(){
+    console.log('Lazy and waiting');
+}, 4000)
 console.log(4);
+setTimeout(() => {
+    console.log('see you later');
+}, 4000)
 console.log(4);
+
+setInterval(() => {             //nisdistro time por eta hote e takbe
+   console.log('do it again and again');
+}, 2000);
