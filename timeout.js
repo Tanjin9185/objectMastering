5cd31654cbcb05062b01eea0281f603329727cc5
Tanjin Ahamed <tanjin15-9185@diu.edu.bd>
2021-02-05 20:35:49 +0600

    ok

---
+++ timeout.js
@@ -0,0 +1,12 @@
+//onno kaj ses hye jawar por e kaj ta krbe
+
+function doSomething(){
+    console.log(3);
+}
+
+console.log(2);
+setTimeout(doSomething, 1000); //onno kaj hote takbe e time er por eta ex
+console.log(4);
+console.log(4);
+console.log(4);
+console.log(4);
